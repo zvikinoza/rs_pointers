afe923b1d12c6932d3bf72ae177e0c703ad0f247
zvikinoza <zvikinoza@gmail.com>
2022-04-07 20:14:56 +0400

    rc done

--- src/rc.rs
+++ src/rc.rs
@@ -1,1 +1,58 @@
-pub struct Rc<T> {}
+use crate::cell::Cell;
+use std::marker::PhantomData;
+use std::ptr::NonNull;
+
+struct RcInner<T> {
+    value: T,
+    ref_count: Cell<usize>,
+}
+pub struct Rc<T> {
+    inner: NonNull<RcInner<T>>,
+    _marker: PhantomData<RcInner<T>>,
+}
+
+impl<T> Rc<T> {
+    pub fn new(value: T) -> Self {
+        let inner = Box::new(RcInner {
+            value,
+            ref_count: Cell::new(1),
+        });
+        Rc {
+            inner: unsafe { NonNull::new_unchecked(Box::into_raw(inner)) },
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<T> std::ops::Deref for Rc<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        &unsafe { self.inner.as_ref() }.value
+    }
+}
+
+impl<T> Clone for Rc<T> {
+    fn clone(&self) -> Self {
+        let inner = unsafe { self.inner.as_ref() };
+        let c = inner.ref_count.get();
+        inner.ref_count.set(c + 1);
+        Rc {
+            inner: self.inner,
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<T> Drop for Rc<T> {
+    fn drop(&mut self) {
+        let inner = unsafe { self.inner.as_ref() };
+        let c = inner.ref_count.get();
+        if c == 1 {
+            #[allow(unused_variables)]
+            let inner = ();
+            let _ = unsafe { Box::from_raw(self.inner.as_ptr()) };
+        } else {
+            inner.ref_count.set(c - 1);
+        }
+    }
+}
